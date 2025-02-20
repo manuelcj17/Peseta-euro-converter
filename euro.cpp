7ed2ce6b955d1e15702d442efd126036ce7fbf82
manuelc17 <167118161+manuelcj17@users.noreply.github.com>
2025-02-20 17:10:04 +0100

    Add files via upload

---
+++ euro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+//Indicamos el valor actual de euro a peseta
+double PTS_EUR = 166.38;
+
+int main(){
+
+//Definimos las variables a convertir
+
+   double euros, pts;
+
+//Confeccionamos el programa
+
+   cout << "Introduzca la cantidad de pesetas: ";
+   cin >> pts;
+   euros = pts/PTS_EUR;
+   cout << pts << " pesetas equivalen a: " << euros << " euros" << endl;
+}
+
+
+
